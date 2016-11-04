817c1366dd92385180c4f7fe6b9001cebb7f0e75
91098785 <91098785@mvusd.net>
2016-11-04 16:43:38 -0700

    first commit

---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/ConcentricLoop.java
@@ -0,0 +1,29 @@
+package org.teachingkidsprogramming.section03ifs;
+
+import org.teachingextensions.logo.Tortoise;
+
+public class ConcentricLoop
+{
+  public static void main(String[] args)
+  {
+    Tortoise.show();
+    Tortoise.setX(150);
+    Tortoise.setY(200);
+    Tortoise.setSpeed(10);
+    for (int i = 0; i < 360; i++)
+    {
+      Tortoise.move(3);
+      Tortoise.turn(1);
+      if (i % 20 == 0)
+      {
+        for (int j = 0; j < 360; j++)
+        {
+          Tortoise.move(1);
+          Tortoise.turn(1);
+          //              Change the width of the line that the tortoise draws to 1/100th of the current line --#9  
+          Tortoise.setPenWidth(j / 100);
+        }
+      }
+    }
+  }
+}
---
+++ TeachingKidsProgramming/src/org/teachingkidsprogramming/section03ifs/HiLowQuiz.java
@@ -0,0 +1,49 @@
+package org.teachingkidsprogramming.section03ifs;
+
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+import org.teachingkidsprogramming.recipes.quizzes.graders.HiLowQuizGrader;
+
+public class HiLowQuiz extends org.teachingkidsprogramming.recipes.quizzes.graders.HiLowQuiz
+{
+  @Override
+  public void question1()
+  {
+    if (Tortoise.getY() == 115)
+    {
+      Tortoise.turn(63);
+    }
+  }
+  @Override
+  public void question2()
+  {
+    if (Tortoise.getX() < Tortoise.getY())
+    {
+      Tortoise.turn(-54);
+    }
+    else
+    {
+      Tortoise.turn(22);
+    }
+  }
+  @Override
+  public void question3()
+  {
+    //  Display the message "elcomeway omehay!"
+    MessageBox.showMessage("elcomeway omehay!");
+  }
+  @Override
+  public void question4()
+  {
+    //  If the Y position of tortoise is greater than 50
+    if (Tortoise.getY() > 50)
+    {
+      Tortoise.turn(-177);
+    }
+    //  Turn the tortoise 177 degrees to the left
+  }
+  public static void main(String[] args)
+  {
+    new HiLowQuizGrader().grade(new HiLowQuiz());
+  }
+}
